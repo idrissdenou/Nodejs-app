08dbbd78775b8a46a203d199e564434c3ee81331
idrissdenou <idrissdenou.devops@gmail.com>
2025-10-18 18:39:37 +0100

    Update SonarQube server URL and credentials

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.236.129.45:9000/',
+  serverUrl: 'http://13.50.224.226:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
-	    //'sonar.password': 'admin',
+	    'sonar.login': '8c7d2eda9621662ac9c1c4fc6bfb1b9f31ae5c4e',
+	    //'sonar.password': 'admin123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
